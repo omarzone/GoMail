24530ab264e4537b084fb315fa740e8ab0b72e98
Omar <omar231120@hotmail.com>
2022-11-13 14:26:52 -0600

    Diseño principal

--- src/test/RunApp.java
+++ src/test/RunApp.java
@@ -1,6 +1,14 @@
 package test;
 
+import views.MainView;
+
 
 public class RunApp {
     
+     public static void main(String args[]) {
+       MainView main = new MainView();
+       main.setVisible(true);
+       main.setLocationRelativeTo(null);
+    }
+
 }
