04feb0bbeb4deae6936f101a18cccc09de2ed2fb
Omar <omar231120@hotmail.com>
2022-11-13 19:11:20 -0600

    Models mail

---
+++ src/models/Mail.java
@@ -0,0 +1,57 @@
+package models;
+
+
+public class Mail {
+    private String sender;
+    private String subject;
+    private String message;
+    private String date;
+    private String time;
+    
+    
+    public Mail(){
+        
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+    
+    
+}
